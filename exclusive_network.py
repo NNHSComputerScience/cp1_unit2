cac2775accf3d2155703f753ec688f77cb6a0a10
Matt Callaghan <mcallaghan@naperville203.org>
2020-05-29 21:59:20 -0500

    Update exclusive_network.py
    Added welcome message

--- exclusive_network.py
+++ exclusive_network.py
@@ -17,20 +17,21 @@ while not password:
 
 if username == "Mike" and password == "secret":
     print ("Hi, Mike.")
-    security = 5
+    security = 1
 elif username == "S.Meier" and password == "civilization":
     print ("Howdy, Sid.")
-    security = 3
+    security = 1
 elif username == "S.Miyamoto" and password == "mariobros":
     print ("What's up, Shigeru?")
-    security = 3
+    security = 1
 elif username == "W.Wright" and password == "thesims":
     print ("How goes it, Will?")
-    security = 3
+    security = 1
 elif username == "guest" or password == "guest":
     print ("Welcome, guest.")
     security = 1
 else:
     print ("Login failed.  You're not so exclusive.\n")
-
-input("\n\nPress the enter key to exit.")
+    
+if security == 1:
+    print("Welcome to the Exclusive Computer Network", username)
