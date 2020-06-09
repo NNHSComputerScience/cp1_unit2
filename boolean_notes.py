26cb816c05f0feca044da8949878be0bb9f4818c
Matt Callaghan <mcallaghan@naperville203.org>
2020-06-09 13:48:29 -0500

    remove functions intro and put in separate file

--- boolean_notes.py
+++ boolean_notes.py
@@ -103,30 +103,4 @@ else:
 if security == 1: 
     print("Welcome to the Exclusive Computer Network", username) 
 
-# function definition - creating your own function
-#   don't need to know yet, just a demo
-def calc_grade(percentage): 
-    """takes in a percentage and prints the appropriate letter grade"""
-    if(percentage >= 90):
-        letter_grade = "A"
-    # this overchecks the value of percentage
-    #   We know that percentage must be < 90 if the
-    #       elif is executed.
-    #elif(percentage >= 80 and percentage < 90):
-    elif(percentage >= 80):
-        letter_grade = "B"
-    elif(percentage >= 70):
-        letter_grade = "C"
-    elif(percentage >= 60):
-        letter_grade = "D"
-    else:
-        letter_grade = "F"
-    
-    print("The grade is " + letter_grade)
-
-# using (calling) our new function 
-calc_grade(85)
-calc_grade(91)
-
-
 input("Press enter to exit.")
