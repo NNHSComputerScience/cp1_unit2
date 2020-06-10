1440421bd5f3346e50e88edf67b2852d03a9b47e
Matt Callaghan <mcallaghan@naperville203.org>
2020-06-10 13:59:30 -0500

    updated inputs to say continue rather than exit

--- while_loop_notes_ starter.py
+++ while_loop_notes_ starter.py
@@ -1,9 +1,9 @@
 # while loop notes
 '''
 while loop, written in English (PSEUDOCODE):
-test the LOOP CONDITION
+while statement tests the LOOP CONDITION
     while condition is True, run this block of code (LOOP BODY)
-    if condition becomes false (SENTINEL VALUE is met), this block is skipped
+    if condition becomes false (SENTINEL VALUE is met), this block is skipped and loop ends
 loop ends and program continues sequentially
 
 # WHILE LOOP -
@@ -20,33 +20,33 @@ loop ends and program continues sequentially
 
 # Common mistake: 
 
-input("\nPress enter to exit.")
+input("\nPress enter to continue.\n")
 
 # Countdown Challenge (on own)
 #   Make a counter that counts down from 10 and then says "BLAST OFF!"
 
 
-input("\nPress enter to exit.")
+input("\nPress enter to continue.\n")
 
 # Count by 5's Challenge(on own)
 #   Count 0-100 by 5's
 #   Print a message to tell the user when the count is half-way done
 
 
-input("\nPress enter to exit.")
+input("\nPress enter to continue.\n")
 
 # die_roller Challenge (partner)
 #   Guess how many rolls it takes to roll a 5?
 
 
 
-input("\nPress enter to exit.")
+input("\nPress enter to continue.\n")
 
 # die_roller2 Challenge (partner)
 #   Display how many evens after 5 rolls of a die.
 
 
-input("\nPress enter to exit.")
+input("\nPress enter to exit.\n")
 
 
 
