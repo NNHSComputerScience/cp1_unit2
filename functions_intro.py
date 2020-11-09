b71f7da25150ab7f7b649af404f546f575ffa1da
Matt Callaghan <mcallaghan@naperville203.org>
2020-11-09 16:48:39 -0600

    Update functions_intro.py

--- functions_intro.py
+++ functions_intro.py
@@ -1,5 +1,15 @@
-# function definition - creating your own function
-#   not required to know yet, this is just a demo
+"""
+# demo of a function for help in understanding the online textbook
+
+# FUNCTION - a named sequence of statements that belong together and accomplish a task.
+
+The syntax for a function definition is:
+def name( parameters (optional) ):
+    statements
+    return statement(optional)
+"""
+
+# FUNCTION DEFIITION - creating your own function
 
 def calc_grade(percentage): 
     """takes in a percentage and prints the appropriate letter grade"""
@@ -17,9 +27,11 @@ def calc_grade(percentage):
         letter_grade = "D"
     else:
         letter_grade = "F"
-    
-    print("The grade is " + letter_grade)
+    # return keyword returns a value from the function, ending the function's execution 
+    return letter_grade
 
-# using (calling) our new function 
-calc_grade(85)
-calc_grade(91)
+# using (CALLING) our new function 
+grade1 = calc_grade(85)
+grade2 = calc_grade(91)
+print("Grade 1", grade1)
+print("Grade 2", grade2)
