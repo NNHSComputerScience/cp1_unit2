ce4912db7e9064441be9d648f7582e39bdd0cc97
Matt Callaghan <mcallaghan@naperville203.org>
2020-11-16 12:13:06 -0600

    Update maitre_d_with_in_operator_starter.py

--- maitre_d_with_in_operator_starter.py
+++ maitre_d_with_in_operator_starter.py
@@ -8,17 +8,21 @@
 
   'in' operator
     Searches in a sequence(e.g. a string is a sequence of characters) for a 
-      value and returns True if found.
-    e.g. the following prints that there's a fun in fundamentals:
-    if "fun" in "fundamentals":
-        print("There's a 'fun' in fundamentals.")
+      value and returns True if found, False if not found.
+    May be combined with the 'not' operator to check if a value is 'not in' a 
+      sequence.
+'''
+# e.g. the following prints that there's a fun in fundamentals:
+if "fun" in "fundamentals":
+    print("There's a 'fun' in fundamentals.")
         
-    e.g. the following prints that there's no i in team: 
-    if "i" in "team":
-        print("There's an 'i' in team.")
-    else:
-        print("There's no 'i' in team.")  
+# e.g. the following prints that there's no i in team: 
+if "i" not in "team":
+    print("There's no 'i' in team.")
+else:
+    print("There's an 'i' in team.")  
 
+'''
 CHALLENGE: Finish the program to check if the patron is a doctor 
           (check for "Dr." and "Doctor").  If they are, tell them that 
           they may be seated right away.  Doctor's don't have to tip if 
