493deab6b6791d85ba27757eed14f6928cda1912
Matt Callaghan <mcallaghan@naperville203.org>
2023-02-23 14:35:53 -0600

    Update boolean_notes.py

--- boolean_notes.py
+++ boolean_notes.py
@@ -1,61 +1,71 @@
-# boolean values and logical operators
+"""
+Name: 
+Title: Unit 2 Notes - boolean values and logical operators
+Description: boolean values and logical operators
+"""
 import random
 
 # 2 BOOLEAN VALUES: True and False (must be capitalized in Python)
-#   test in shell...
-'''
->>> True
-True
->>> False
-False
->>> true
-Traceback (most recent call last):
-  File "<pyshell#2>", line 1, in <module>
-    true
-NameError: name 'true' is not defined
->>> type(True)
-<class 'bool'>
->>> type("False")
-<class 'str'>
->>>
-'''
+#   in console...
+print(True)
+print(False)
 
-# a BOOLEAN EXPRESSION is an expression that evaluates to a boolean value.
-#  in shell...
-'''
->>> 5 == 5
-True
->>> 5 != 5
-False
->>> 5 < 10
-True
->>> 5 >= 10
-False
->>> 
-'''
+var = True
+print(var)
+
+# a BOOLEAN EXPRESSION is an expression that evaluates to either True or False.
+#  in console...
+print(5 == 5)  # True
+print(5 != 5)  # False
+
+# CHALLENGE: Generate a random number between 1 and 13 and print "Your number is lucky." if the number is 7, 11, or 13.
+
+num = random.randint(1, 13)
+print(num)
+if num == 7:
+	print("Your number is lucky.")
+elif num == 11:
+	print("Your number is lucky.")
+elif num == 13:
+	print("Your number is lucky.")
+else:
+	print("Your number is not lucky")
 
 # LOGICAL OPERATORS: and, or, not
 #   order of precendence is: not, and, or
-#   https://runestone.academy/runestone/static/thinkcspy/Selection/PrecedenceofOperators.html
 
-# CHALLENGE: Generate a random number between 1 and 13 and
-#            print "Your number is lucky." if the number is 7, 11, or 13.
-
-lucky_num = random.randint(1,13)
-print(lucky_num)
-if lucky_num == 7 or lucky_num == 11 or lucky_num == 13:
-    print("Your number is lucky.")
-# if lucky_num == 7 or 11 or 13:  <-- always True!
-
-#  non-zero numbers and non-empty strings are evaluated as True
-if 1: # and "Hi" and 5.5
-    if "Hi":
-        if 5.5:
-            print("All evaluated to True.")
-if 0 or 0.0 or "":
-    print("At least one evaluated to True.")
+if num == 7 or num == 11 or num == 13:
+	print("Your number is lucky.")
 else:
-    print("All evaluated to False.")
+	print("Your number is not lucky.")
+
+# TRAP: 
+if num == 7 or 11 or 13:  # 11 and 13 are evaluated as expressions
+	print("Your number is lucky.")
+else:
+	print("Your number is not lucky.")
+
+# non-zero numbers are evaluated as True; zero is evaluated as False
+if 0:
+	print("Evaluated as True")
+else:
+	print("Evaluated as False")
+
+# any non-empty string value is evaluated as True; empty strings are False
+if "":
+	print("Monty Evaluated as True")
+else:
+	print("Monty Evaluated as False")
+
+# Nesting if statements
+num = 10
+if "1" in str(num):
+	if "0" in str(num):	
+		print("One and Zero zero are in the number")
+  else:
+    print("One and Zero are not in the number")  # BUG
+  
+# nested code only runs if the outer if statement is True
 
 # CHALLENGE: Exclusive Network
 '''
@@ -74,33 +84,3 @@ Extension challenges:
 - What other features would an exclusive network have?
 
 '''
-
-# Exclusive Network
-# Demonstrates logical operators in conditions
-#   and, or, not
-
-print ("\tExclusive Computer Network")
-print ("\t\tMembers only!\n")
-
-security = 0
-
-username = input("Username: ") 
-password = input("Password: ") 
-
-if username == "a_user" and password == "itsasecret":
-    security = 1  
-elif username == "s_meier" and password == "civilization":
-    security = 1 
-elif username == "s_miyamoto" and password == "mariobros":
-    security = 1 
-elif username == "w_wright" and password == "thesims":
-    security = 1 
-elif username == "guest" or password == "guest":
-    security = 1 
-else:
-    print ("Login failed.  You're not so exclusive.\n")
-
-if security == 1: 
-    print("Welcome to the Exclusive Computer Network", username) 
-
-input("Press enter to exit.")
